ec62149cd3bd2abeec3f47bf4ba8e930542431b7
saksham-accolite <saksham.goyal@accolitedigital.com>
2021-01-17 23:43:26 +0530

    Angular Assn

---
+++ AngularAssn/au-sessions/src/app/mock-sessions.ts
@@ -0,0 +1,24 @@
+import { Session } from './session';
+
+export const SESSIONS: Session[] = [
+    {
+        name: 'angular1',
+        trainer: 'trainer1',
+        date: new Date("2019-01-14")
+    },
+    {
+        name: 'angular2',
+        trainer: 'trainer2',
+        date: new Date("2019-01-15")
+    },
+    {
+        name: 'react',
+        trainer: 'trainer3',
+        date: new Date("2019-01-16")
+    },
+    {
+        name: 'react-native',
+        trainer: 'trainer4',
+        date: new Date("2019-01-17")
+    }
+  ];
